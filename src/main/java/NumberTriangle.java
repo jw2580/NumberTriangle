05c6484ed4b5c027a7e22bb78b91ed6d6b62b696
wujinfeng <jim.wu2580@gmail.com>
2025-09-28 23:34:33 -0400

    implemented

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -88,8 +88,18 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
-        return -1;
+        NumberTriangle curr = this;
+        for (int i = 0; i < path.length(); i++) {
+            char c = path.charAt(i);
+
+            if (c == 'l') {
+                curr = curr.left;
+            }
+            else if (c == 'r') {
+                curr = curr.right;
+            }
+        }
+        return curr.getRoot();
     }
 
     /** Read in the NumberTriangle structure from a file.
